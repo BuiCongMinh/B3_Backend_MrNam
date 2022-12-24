b60a112b1ba7888afb82fa2f5d480c3676ff411e
BuiCongMinh <65665940+BuiCongMinh@users.noreply.github.com>
2022-12-25 00:03:38 +0700

    BaiTapVeNha MrNam

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,20 +1,39 @@
 const Product = require('../models/Product')
 
-const postAPIProduct = (req,res)=>{
-    console.log(req.body);
-    Product.create({ 
-        Name:req.body.Name,
+const postAPIProduct = (req, res) => {
+    Product.create({
+        Name: req.body.Name,
         Quatity: req.body.Quatity,
-        comment:req.body.comment
+        comment: req.body.comment
     })
-    .then(data=>{res.status(200).json(data)})
-    .catch(er =>{ res.status(500).json(er)})
+        .then(data => { res.status(200).json(data) })
+        .catch(er => { res.status(500).json(er) })
 }
 
-const getAllAPIProduct = (req,res)=>{
+const getAllAPIProduct = (req, res) => {
     Product.find()
-    .then(data=>{res.status(200).json(data)})
-    .catch(er=>{res.status(500).json(er)})
+        .then(data => { res.status(200).json(data) })
+        .catch(er => { res.status(500).json(er) })
 }
 
-module.exports = {postAPIProduct, getAllAPIProduct}
+const getOneAPIProduct = (req, res) => {
+    Product.findOne({ Name: req.params.name })
+        .then(data => { res.status(200).json(data) })
+        .catch(er => { res.status(500).json({ mesage: 'server er', er }) })
+}
+
+
+const patchProduct = (req, res) => {
+    Product.updateOne({ Name: req.params.name }, req.body)
+        .then(data => { res.status(200).json(data) })
+        .catch(er => { res.status(500).json({ mesage: 'server er', er }) })
+}
+
+
+const deleteProduct = (req, res) => {
+    Product.deleteOne({ Name: req.body.Name })
+        .then(data => { res.status(200).json(data) })
+        .catch(er => { res.status(500).json({ mesage: 'server er', er }) })
+}
+
+module.exports = { postAPIProduct, getAllAPIProduct, getOneAPIProduct, patchProduct, deleteProduct }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const {postAPIComment, getAllAPIComment} = require('../controller/comment.controller')
-const {postAPIProduct,getAllAPIProduct} = require('../controller/product.controller')
+const productController = require('../controller/product.controller')
 
 // rest API Comment 
 router.get('/get-all/comment',getAllAPIComment)
@@ -8,7 +8,10 @@ router.post('/post/comment',postAPIComment)
 
 
 // rest API Product
-router.post('/post/product',postAPIProduct)
-router.get('/get-all/product',getAllAPIProduct)
+router.post('/post/product',productController.postAPIProduct)
+router.get('/get-all/product',productController.getAllAPIProduct)
+router.get('/get/product/:name',productController.getOneAPIProduct)
+router.patch('/patch/product/:name',productController.patchProduct)
+router.delete('/delete/product', productController.deleteProduct)
 
 module.exports = router
